fdc68e8d353da7ab015bb74f2152089aeb120c7e
Yehor Tertyshnyk <egor.tertyshnyk@gmail.com>
2026-09-25 12:43:34 +0900

    More edited files from astro template.

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Yehor Tertyshnyk';
+export const SITE_DESCRIPTION = 'Personal website to share my contribution in open source, research and photography';
